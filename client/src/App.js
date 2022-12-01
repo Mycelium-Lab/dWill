c19b286bddf1ec97cf3578b6ba9c95061884e1f8
Alex <jivoy1057@mail.ru>
2022-12-01 19:34:10 +0300

    edits

--- client/src/App.js
+++ client/src/App.js
@@ -153,6 +153,43 @@ class App extends Component {
       // Catch any errors for any of the above operations.
       console.error(error);
     }
+
+    function canvas() {
+      const cnv = document.querySelector('#cnv');
+      //Get context and screen size;
+      var ctx = cnv.getContext("2d");
+      var W = window.innerWidth;
+      var H = window.innerHeight;
+
+      //Set Canvas and Background Color;
+      cnv.width = W;
+      cnv.height = H;
+      ctx.fillStyle = "#112";
+      ctx.fillRect(0, 0, W, H);
+
+      //Glow effect;
+      ctx.shadowBlur = 10;
+      ctx.shadowColor = "white";
+
+      function animate() {
+        //Random position and size of stars;
+        let x = W * Math.random();
+        let y = H * Math.random();
+        let r = 2.5 * Math.random();
+
+        //Draw the stars;
+        ctx.beginPath();
+        ctx.fillStyle = "white";
+        ctx.arc(x, y, r, 0, Math.PI * 2);
+        ctx.fill();
+
+        //Using setTimeout instead of window.requestAnimationFrame for slower speed... window.requestAnimationFrame is approximately equal to setTimeout(func, 17);
+        setTimeout(animate, 100);
+      }
+      animate();
+
+    }
+    canvas();
   };
 
   numberWithSpaces(x) {
@@ -271,6 +308,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <canvas id="cnv"></canvas>
         <header className="header _container">
           <div className='header_boxes'>
             <div className="header_boxes-col">
