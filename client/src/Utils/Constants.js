5cd237204774e1b2e62805f8dbfa13912f6d636a
E.G <60756104+elolpuer@users.noreply.github.com>
2023-01-09 18:33:08 +0300

    eth node new

--- client/src/Utils/Constants.js
+++ client/src/Utils/Constants.js
@@ -32,11 +32,11 @@ export const chainIDs = {
     OptimismMainnet: 10
 }
 export const chainRPCURL = {
-    Polygon: 'https://polygon-rpc.com',
+    Polygon: 'https://polygon.llamarpc.com',
     Mumbai: 'https://rpc-mumbai.maticvigil.com',
     BinanceTestnet: 'https://data-seed-prebsc-1-s1.binance.org:8545',
     BinanceMainnet: 'https://bsc-dataseed.binance.org',
-    EthereumMainnet: 'https://eth-mainnet.g.alchemy.com/v2/qH54waWkb7SvypR0UXSteTWZ5lvyFlJ0',
+    EthereumMainnet: 'https://eth.llamarpc.com',
     Goerli: 'https://goerli.infura.io/v3/',
     AvalancheMainnet: 'https://rpc.ankr.com/avalanche',
     ArbitrumMainnet: 'https://rpc.ankr.com/arbitrum',
