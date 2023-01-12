5644aaddbfb53a667b40ccf64044d76d1807664c
E.G <60756104+elolpuer@users.noreply.github.com>
2023-01-12 09:45:54 +0300

    update link in text docs

--- client/src/Main/Main.js
+++ client/src/Main/Main.js
@@ -32,7 +32,7 @@ class Main extends Component {
                 {/* <h1 className="block-one__title">Hello!</h1> */}
                 <h3 className="block-one">dWill - the first fully decentralized way to bequeath your cryptocurrency.</h3>
                 <p className="block-two">dWill - the first decentralized non-custodial inheritance protocol. With dWill, you can bequeath any ERC-20 tokens from your cryptocurrency wallet to the trusted wallets of your family and friends (or, for example, to your reserve wallet)</p>
-                <p className="block-two">Thanks to smart contract technology, dWill works completely in a decentralized way, reliably and autonomously. No one (no one at all, not even the project team) will have access to the funds you bequeathed.&nbsp;<a href='#' target="_blank" rel="noreferrer">Read more about how it works.</a>
+                <p className="block-two">Thanks to smart contract technology, dWill works completely in a decentralized way, reliably and autonomously. No one (no one at all, not even the project team) will have access to the funds you bequeathed.&nbsp;<a href='https://dwill.slite.page/p/yFyU0Vhz-TJakC/dWIll' target="_blank" rel="noreferrer">Read more about how it works.</a>
                 </p>
                 {
                     (this.props.inheritancesLength === 0 || this.props.signer === null)
